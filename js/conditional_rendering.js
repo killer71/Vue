606b526e7e25c385f3e033e13deb64e664c87a19
killer71 <magaky71@gmail.com>
2019-05-02 23:05:43 +0300

    conditional rendering

---
+++ js/conditional_rendering.js
@@ -0,0 +1,20 @@
+let app1 = new Vue({
+    el: "#app-1",
+    data: {
+        awesome: true
+    }
+});
+
+let app2 = new Vue({
+    el: "#app-2",
+    data: {
+        awesome: false
+    }
+});
+
+let app3 = new Vue({
+    el: "#app-3",
+    data: {
+        
+    }
+});
